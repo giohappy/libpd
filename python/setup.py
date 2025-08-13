40bd8a5d11025385a9d278548b47ea8653d17c8b
Giovanni Allegri <giohappy@gmail.com>
2025-08-13 20:08:25 +0200

    Add MSVC support to python bindings

--- python/setup.py
+++ python/setup.py
@@ -7,31 +7,34 @@ pd_defines = [
   ('PD', 1),
   ('USEAPI_DUMMY', 1),
   ('PD_INTERNAL', 1),
-  ('HAVE_UNISTD_H', 1),
   ('LIBPD_EXTRA', 1)
 ]
+pd_undefs = []
 pd_libraries = [
-  'm',
-  'pthread'
+  'pthread_static_lib'
 ]
 
 # replicate libpd/Makefile PLATFORM_CFLAGS & LDFLAGS
 if sys.platform.startswith('darwin'):
+  pd_defines.append(('HAVE_UNISTD_H', 1))
   pd_defines.append(('HAVE_ALLOCA_H', 1))
   pd_defines.append(('HAVE_MACHINE_ENDIAN_H', 1))
   pd_defines.append(('_DARWIN_C_SOURCE', 1))
   pd_defines.append(('_DARWIN_UNLIMITED_SELECT', 1))
   pd_defines.append(('FD_SETSIZE', 10240))
   pd_defines.append(('HAVE_LIBDL', 1))
+  pd_libraries.append('m')
   pd_libraries.append('dl')
 elif sys.platform.startswith('win32') or sys.platform.startswith('msys'):
   # windows doesn't have alloca.h, endian.h, or libdl
   pd_libraries.append('ws2_32') # winsock
 else: # assume posix env...
+  pd_defines.append(('HAVE_UNISTD_H', 1))
   pd_defines.append(('HAVE_ENDIAN_H', 1))
   if sys.platform.startswith('linux'):
     pd_defines.append(('HAVE_ALLOCA_H', 1))
     pd_defines.append(('HAVE_LIBDL', 1))
+    pd_libraries.append('m')
     pd_libraries.append('dl')
 
 setup(name='pypdlib',
@@ -44,8 +47,13 @@ setup(name='pypdlib',
                   define_macros = pd_defines,
                   include_dirs = [
                     '../libpd_wrapper',
-                    '../pure-data/src'
+                    '../pure-data/src',
+                    "../pthread-win32"
                   ],
+                  library_dirs = [
+                    "../pthread-win32/windows/VS2022/bin/RELEASE-Unicode-64bit-x64"
+                  ],
+                  extra_compile_args=['/std:c11', '/experimental:c11atomics'],
                   libraries = pd_libraries,
                   sources = [
                     'pylibpd.i',
